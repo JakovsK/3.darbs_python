7a860045412d55ea86829984d8c21c3b96ba5c4d
JakovsK <jkotovs@gmail.com>
2022-04-15 14:32:08 +0300

    mans darbs

--- 4uzdevums.py
+++ 4uzdevums.py
@@ -3,5 +3,5 @@ Uzrakstiet programmu Python, lai dotajā sarakstā saskaitītu
 cik skaitļi 4 ir  dotajā sarakstā.
   """
 
-list=[4,6,13,5,6,8,96,6]
-print(list.count(6))
+list=[4,6,13,5,6,4,8,96,6]
+print(list.count(4))
